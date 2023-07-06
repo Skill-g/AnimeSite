513f6a1d236da169a52cf3acf84f3a6cafb1ed70
Skill-g <nikitaepifantsevv@gmail.com>
2023-07-07 02:48:58 +0600

    anime web site

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const search = document.getElementById('search')
+const violethoverd = document.getElementsByClassName('violetdiscription')
+const violethover = document.getElementsByClassName('violet')
+
+for (var i = 0; i < violethover.length; i++) {
+    violethover[i].addEventListener('mouseover', function() {
+        for (var j = 0; j < violethover.length; j++) {
+            violethoverd[j].style.opacity = '1';
+        }
+    });
+
+    violethover[i].addEventListener('mouseout', function() {
+        for (var j = 0; j < violethover.length; j++) {
+            violethoverd[j].style.opacity = '0';
+        }
+    });
+}
